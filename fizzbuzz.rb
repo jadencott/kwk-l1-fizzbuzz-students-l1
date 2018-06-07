4cef560d3e2607f90bdd01b83596c3e62a287418
Jaden <jcottorone21405@gmail.com>
2018-06-07 00:12:52 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+# if #{number} / 3, then puts fizz
+#elseif #{number} /5, then puts buzz
+#else #{number} /3 && #{number} /5, the puts FizzBuzz
+
+
+# def 
+# describe "fizzbuzz" do
+#   it 'returns "Fizz" when the number is divisible by 3' do
+#     fizz_3 = fizzbuzz(3)
+ 
+#     expect(fizz_3).to eq("Fizz")
+#   end
+# end
+
+
+
+describe "fizzbuzz" do 
+  
+  
---
+++ fizzbuzztwo.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+puts "fizzbuzz"
+  elsif number % 3 == 0
+  puts "fizz"
+elsif number % 5 == 0 
+puts "buzz"
+else 
+  puts "nil"
+
+end
+end
+
+fizzbuzz(15)
